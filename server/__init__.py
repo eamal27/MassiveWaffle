bac0d9e77a71bfa5c2a3fd9ff4408b89a26208d0
Caleb Phillips <calebrphillips@gmail.com>
2017-01-28 12:44:42 -0500

    Implement reddit api connection and average score for given submission id

--- server/__init__.py
+++ server/__init__.py
@@ -1,19 +1,29 @@
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from flask import Flask
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import praw
+
+reddit = praw.Reddit(client_id='KKt5S-SgmLgf4g',
+                     client_secret='B8FpLDncJ5fv1FfKhxDBKDyiTD4',
+                     user_agent='web:com.caleb.massivewaffle:v1.0.0')
+
 
 app = Flask(__name__)
 
 sid = SentimentIntensityAnalyzer()
 
-@app.route('/')
-def hello():
-    return 'hello world'
+@app.route('/reddit/<id>')
+def score(submission_id):
+    submission = reddit.submission(id=submission_id)
+    comments = [c.body for c in submission.comments.list()]
+    scores = [score_sentence(comment) for comment in comments]
+    avg = sum(scores)/len(scores)
+    return avg
+
 
 def score_sentence(sentence):
     scores = sid.polarity_scores(sentence)
     return scores['compound']
 
 if __name__ == "__main__":
-    test_sentence = 'this is fucked'
-    score = score_sentence(test_sentence)
-    print(score)
+    x = score('5qmtpo')
+    print(x)
