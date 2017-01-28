0299ac26317f0b43dce07684559b70ad175aca20
Caleb Phillips <calebrphillips@gmail.com>
2017-01-28 12:01:53 -0500

    Implement function to score sentences

--- server/__init__.py
+++ server/__init__.py
@@ -1,8 +1,19 @@
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from flask import Flask
 
 app = Flask(__name__)
 
+sid = SentimentIntensityAnalyzer()
 
 @app.route('/')
 def hello():
     return 'hello world'
+
+def score_sentence(sentence):
+    scores = sid.polarity_scores(sentence)
+    return scores['compound']
+
+if __name__ == "__main__":
+    test_sentence = 'this is fucked'
+    score = score_sentence(test_sentence)
+    print(score)
