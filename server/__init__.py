ef91f3f2f5478dc55c4c408a7122b27b1f41c800
Caleb Phillips <calebrphillips@gmail.com>
2017-01-28 13:24:47 -0500

    Add HN endpoint for scoring comments

--- server/__init__.py
+++ server/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import praw
+from hackernews import HackerNews
 
 reddit = praw.Reddit(client_id='KKt5S-SgmLgf4g',
                      client_secret='B8FpLDncJ5fv1FfKhxDBKDyiTD4',
                      user_agent='web:com.caleb.massivewaffle:v1.0.0')
+
+hn = HackerNews()
 
 
 app = Flask(__name__)
@@ -12,18 +15,27 @@ app = Flask(__name__)
 sid = SentimentIntensityAnalyzer()
 
 @app.route('/reddit/<id>')
-def score(submission_id):
+def score_reddit(submission_id):
     submission = reddit.submission(id=submission_id)
     comments = [c.body for c in submission.comments.list()]
     scores = [score_sentence(comment) for comment in comments]
-    avg = sum(scores)/len(scores)
-    return avg
+    return avg(scores)
 
+@app.route('/hn/<id>')
+def score_hn(page_id):
+    page = hn.get_item(page_id)
+    scores = [score_sentence(hn.get_item(x).text) for x in page.kids]
+    return avg(scores)
 
 def score_sentence(sentence):
+    if sentence is None: 
+        return 0
     scores = sid.polarity_scores(sentence)
     return scores['compound']
 
+def avg(scores):
+    return sum(scores)/len(scores)
+
 if __name__ == "__main__":
-    x = score('5qmtpo')
+    x = score_hn('13506670')
     print(x)
